cd312a57746080be1394a00225204cc2badea249
hbs <hbsowo58@gmail.com>
2020-04-20 22:37:41 +0900

    feat: add component showing todolist

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>list</div>
+  <section>
+    <ul>
+      <li v-for="todoItem in todoItems" v-bind:key="todoItem">{{todoItem}}</li>
+    </ul>
+  </section>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      todoItems: []
+    };
+  },
+  created() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        this.todoItems.push(localStorage.key(i));
+      }
+    }
+  }
+};
 </script>
 
 <style></style>
